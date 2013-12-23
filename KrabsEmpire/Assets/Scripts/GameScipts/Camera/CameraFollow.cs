8aa4345e3c35b27dc33122d1833ec78d654abf36
AndreasOxinos <oxinosa@hotmail.com>
2013-12-23 18:30:53 +0200

    Initial Commit
    Initial

---
+++ KrabsEmpire/Assets/Scripts/GameScipts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform playerToFollow;
+
+    public int CameraOffSetX = 2;
+    public int CameraOffSetY = 2;
+
+    void Update()
+    {
+        if (playerToFollow == null)
+        {
+            playerToFollow = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        var targetX = Mathf.Lerp(transform.position.x, playerToFollow.position.x, CameraOffSetX * Time.deltaTime);
+        var targetY = Mathf.Lerp(transform.position.y, playerToFollow.position.y, CameraOffSetY * Time.deltaTime);
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+        //transform.position = new Vector3(playerToFollow.transform.position.x, playerToFollow.transform.position.y, transform.position.z);
+        
+    }
+}
---
+++ KrabsEmpire/Assets/Scripts/GameScipts/Character/BlueHairLadyController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueHairLadyController : MonoBehaviour
+{
+    private string AxisX = "Horizontal";
+    private string AxisY = "Vertical";
+    private Animator anim;
+    public int Speed = 5;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        anim.SetFloat("SpeedX", Input.GetAxis(AxisX));
+        anim.SetFloat("SpeedY", Input.GetAxis(AxisY));
+
+
+        MoveX();
+        MoveY();
+    }
+
+
+    void MoveX()
+    {
+        if (Mathf.Abs(Input.GetAxis(AxisX)) > 0)
+        {
+            if(Input.GetAxis(AxisX)>0)
+            {
+                anim.SetFloat("Direction", 4);
+            }
+            if(Input.GetAxis(AxisX)<0)
+            {
+                anim.SetFloat("Direction", 3);
+            }
+            
+            transform.position += transform.right * Input.GetAxis(AxisX) * Speed * Time.deltaTime;
+        }
+    }
+
+    void MoveY()
+    {
+        if (Mathf.Abs(Input.GetAxis(AxisY)) > 0)
+        {
+            if(Input.GetAxis(AxisY)>0)
+            {
+                anim.SetFloat("Direction", 2);
+            }
+            if(Input.GetAxis(AxisY)<0)
+            {
+                anim.SetFloat("Direction", 1);
+            }
+            
+            transform.position += transform.up * Input.GetAxis(AxisY) * Speed * Time.deltaTime;
+        }
+    }
+}
