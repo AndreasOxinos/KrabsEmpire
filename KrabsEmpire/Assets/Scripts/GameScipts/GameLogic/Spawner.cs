b9181db56daa7f44f8ab23a2a38ea0ad3a14c20b
Andreas Oxinos <oxinosa@hotmail.com>
2013-12-24 13:14:28 +0200

    Paragon City Created
    Paragon city

---
+++ KrabsEmpire/Assets/Scripts/GameScipts/GameLogic/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+    void Start()
+    {
+        GameObject _char = (GameObject)Instantiate(Resources.Load("character"));
+        _char.transform.position = new Vector2(transform.position.x, transform.position.y);
+    }
+
+}
---
+++ KrabsEmpire/Assets/Scripts/GameScipts/InteractiveObjects/Buildings/BuildingController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingController : MonoBehaviour
+{
+
+    public string NextLevel = "";
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Application.LoadLevel(NextLevel);
+    }
+}
